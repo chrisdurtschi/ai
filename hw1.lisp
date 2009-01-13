55b92e14e1bb0dd99e79a3e9d5aa59c71a5cb5e3
Christopher Durtschi <pherph@gmail.com>
2009-01-12 21:27:00 -0700

    Completed reverse and mirror problems

--- hw1.lisp
+++ hw1.lisp
@@ -1,23 +1,40 @@
-(defun reversi (lst)
+(defun rev (lst)
   "Takes a list and returns the atoms in reverse order"
+  (drev lst nil)
+)
+
+(defun drev (src dest)
+  "Construct a list, moving elements of src to dest"
   (cond
-    ((null lst) nil)
-    (t (cons (first (last lst)) (reversi (butlast lst))))
+    ((null src) dest)
+    (t (drev (rest src) (cons (first src) dest)))
   )
 )
 
-(defun reversi* (lst)
+(defun rev* (lst)
   "Takes a list and creates a mirror image of it"
+  (drev* lst nil)
+)
+
+(defun drev* (src dest)
+  "Construct a list, moving elements of src to dest, going into sub lists"
   (cond
-    ((null lst) nil)
-    ((atom (first (last lst))) (cons (first (last lst)) (reversi* (butlast lst))))
-    (t (cons (reversi* (first (last lst))) (reversi* (butlast lst))))
+    ((null src) dest)
+    ((listp (first src))
+      (drev* (rest src) (cons (drev* (first src) nil) dest))
+    )
+    (t (drev* (rest src) (cons (first src) dest)))
   )
 )
 
-(defun towers (numdiscs start finish extra)
-  "This function will solve the Towers of Hanoi problem"
+(defun towers (discs start finish extra)
+  "Towers of Hanoi"
   (cond
-    ((> discs 1))
+    ((= discs 1) (princ "Move from ") (princ ) (princ " to ") (princ ) (terpri))
+    (t 
+      (towers (- discs 1) start finish extra) 
+      (towers 1 start finish extra) 
+      (towers (- discs 1) start finish extra)
+    )
   )
 )
