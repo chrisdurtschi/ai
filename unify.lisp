1c3bd5ae3c6a55cb58dc1674a01005643aec1284
robotapocalypse <pherph@gmail.com>
2009-02-02 22:30:36 -0700

    Modified unify; still doesn't work

--- unify.lisp
+++ unify.lisp
@@ -1,3 +1,8 @@
+(defun pred (&rest args)
+  "Creates a predicate expression from the arguments passed in."
+  (cons (first args) (rest args))
+)
+
 (defun constantize (sym)
   "Gives the symbol the property of being a constant"
   (setf (get sym 'termtype) 'constant)
@@ -7,7 +12,7 @@
 (defun isconstant (sym)
   "Determines if the symbol is a constant"
   (cond
-    ((equal (get sym 'termtype) 'constant) t)
+    ((and (atom sym) (equal (get sym 'termtype) 'constant)) t)
     (t nil)
   )
 )
@@ -21,46 +26,74 @@
 (defun isvariable (sym)
   "Determines if the symbol is a variable"
   (cond
-    ((equal (get sym 'termtype) 'variable) t)
+    ((and (atom sym) (equal (get sym 'termtype) 'variable)) t)
     (t nil)
   )
 )
 
-(defun applysub (sub exp newexp)
-  "Documentation for applysub."
+(defun applysubrec (sub exp newexp)
+  "Apply a substitution to the remainder of an expression."
   (cond
-    ((null exp) newexp)
-    ((lisp (first exp) (applysub sub)))
-    (t (if (= (first exp) (first sub)) (cons (second sub) newexp) (cons (first exp) newexp)))
+    ((null exp) (reverse newexp))
+    ((listp (first exp)) (applysubrec sub (rest exp) (cons (applysubrec sub (first exp) nil) newexp)))
+    (t (if (equal (first exp) (first sub)) 
+      (applysubrec sub (rest exp) (cons (second sub) newexp))
+      (applysubrec sub (rest exp) (cons (first exp) newexp)))
+    )
   )
+)
+
+(defun applysub (sub exp)
+  "Apply a substitution to the remainder of an expression."
+  (applysubrec sub exp nil)
 )
 
 (defun unify (exp1 exp2 sublist)
   "Unifies two predicate calculus expressions, returning a substitution list"
   (cond
+    ; We've processed all the elements of the predicates,
+    ; return substitution list
     ((and (null exp1) (null exp2)) sublist) 
-    ((if (and (isconstant exp1) (isconstant exp2) (equal exp1 exp2)) nil 'fail))
+    ((and (isconstant exp1) (isconstant exp2) (equal exp1 exp2)) nil)
     ((isvariable exp1)
-      (cond 
-        ((member exp1 exp2) 'fail)
+      (cond
+        ((or (and (atom exp2) (equal exp1 exp2)) (and (listp exp2) (member exp1 exp2))) 'fail)
         (t (cons (list exp1 exp2) sublist))
       )
     )
     ((isvariable exp2)
       (cond 
-        ((member exp2 exp1) 'fail)
-        (t (list exp2 exp1))
+        ((or (and (atom exp1) (equal exp2 exp1)) (and (listp exp1) (member exp2 exp1))) 'fail)
+        (t (cons (list exp2 exp1) sublist))
       )
     )
     ; if either exp1 or exp2 are empty, return FAIL
     ((or (null exp1) (null exp2)) 'fail)
     ; recursive case
     (t
-      (setq sub1 (unify (first exp1) (first exp2)))
+      (setq sub1 (unify (first exp1) (first exp2) sublist))
       (cond
         ((equal sub1 'fail) 'fail)
-        (t (setq sub2 ))
+        (t 
+          (setq sub2 (unify (applysub sub1 (rest exp1)) (applysub sub1 (rest exp2)) sublist))
+          (cond
+            ((equal sub2 'fail) 'fail)
+            (t (cons sub2 (cons sub1 sublist)))
+          )
+        )
       )
     )
   )
 )
+
+(trace unify)
+(trace applysub)
+(trace applysubrec)
+(trace constantize)
+(trace isconstant)
+(trace variablize)
+(trace isvariable)
+(trace pred)
+
+
+(unify (pred (constantize 'p) (constantize 'bob) (variablize 'x)) (pred (constantize 'p) (variablize 'y) (constantize 'jill)) nil)
